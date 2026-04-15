04d3b913e1d2a1af39c98a9dd95672ed34e1f442
Nikittaaaaa <162281255+Nikittaaaaa@users.noreply.github.com>
2026-04-15 13:51:14 +0300

    Add files via upload

---
+++ Basic Syntax/String Templates - Expressions/src/Task.kt
@@ -0,0 +1,6 @@
+fun printMath(): String {
+    val a = 2
+    val b = 2
+    return "$a + $b = ${a + b}"
+
+}
---
+++ Basic Syntax/Type Safety/src/Task.kt
@@ -0,0 +1,6 @@
+fun getScoreMessage(): String {
+    var score = 100
+    // score = "100" // Це викличе помилку компіляції!
+    return "Your score is $score"
+
+}
